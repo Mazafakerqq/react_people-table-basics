16ac4c906cdd70a021f5cb8d8841268bc3477a4e
Dmytro Kolesnikov <dimon4ik1231731@gmail.com>
2025-04-13 21:44:17 +0300

    solution2

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
 import './App.scss';
 import React from 'react';
 import { NavBar } from './components/Loader/NavBar';
-import { Navigate, Route, Routes } from 'react-router-dom';
+import { Navigate, HashRouter, Route, Routes } from 'react-router-dom';
 import { HomePage } from './components/Loader/HomePage';
 import { PeoplePage } from './components/Loader/PeoplePage';
 import { PageNotFound } from './components/Loader/PageNotFound';
 
 export const App = () => (
-  <div data-cy="app">
-    <NavBar />
+  <HashRouter>
+    <div data-cy="app">
+      <NavBar />
 
-    <main className="section">
-      <Routes>
-        <Route index element={<HomePage />} />
-        <Route path="/home" element={<Navigate to="/" />} />
-        <Route path="people">
-          <Route index element={<PeoplePage />} />
-          <Route path=":slug" element={<PeoplePage />} />
-        </Route>
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
-    </main>
-  </div>
+      <main className="section">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/home" element={<Navigate to="/" />} />
+          <Route path="people">
+            <Route index element={<PeoplePage />} />
+            <Route path=":slug" element={<PeoplePage />} />
+          </Route>
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </main>
+    </div>
+  </HashRouter>
 );
